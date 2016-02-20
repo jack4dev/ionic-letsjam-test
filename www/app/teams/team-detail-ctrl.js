e1af1468fb03049239c53523ce583de798016d5f
Marco Alvarado <marcoantonioam9@gmail.com>
2016-02-20 11:33:54 -0700

    Navigation and Routing module completed

---
+++ www/app/teams/team-detail-ctrl.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+  angular.module('eliteApp').controller('teamDetailCtrl', teamDetailCtrl);
+
+  function teamDetailCtrl($stateParams) {
+    var vm = this;
+  }
+})();
